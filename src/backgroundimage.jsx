523924977fb32ec77f1ba80f372559bc812155ed
adnan-jamil-works <74920961+adnan-jamil-works@users.noreply.github.com>
2025-10-22 09:07:07 -0700

    Update backgroundimage.jsx

--- src/backgroundimage.jsx
+++ src/backgroundimage.jsx
@@ -3,8 +3,9 @@ import TG from "./assets/TG.jpg";
 
 function backgroundimage() {
   return (
-    <div className="-z-10 overflow-hidden absolute top-0 left-0">
-      <img src={TG} />;
+    <div className="-z-10 overflow-hidden absolute top-0 left-0 ">
+      <img src={TG} alt="Background" className="w-full h-full object-cover" />
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#0A192F]/90 to-[#0A192F]/60"></div>
     </div>
   );
 }
